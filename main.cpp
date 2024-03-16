d4c3c09fae8ee706dd9869c3dbc6362d74233caf
Muhammad Saad <saadydev@gmail.com>
2024-03-16 03:52:29 +0000

    started doing graphs

--- main.cpp
+++ main.cpp
@@ -4,103 +4,35 @@
 #include <limits.h>
 #include <string>
 #include <algorithm>  // Add this line for transform
+#include<unordered_map>
+#include<list>
 using namespace std;
 
-class Heap{
+class Graph{
 public:
-vector<int> arr;
-int size = 0;
-Heap(){
-  size = 0;
-  arr[0] = -1;
+unordered_map<int,list<int>> mp;
+void insertEdge(int n1,int n2,bool status){
+  //status 0 for undirected and 1 for directed 
+  mp[n1].push_back(n2);
+  if(status) mp[n2].push_back(n1);
 }
-void insert(int data){
-  size++;
-  int index = size;
-  arr[index] = data;
-  while(index>1){
-    int parent = index/2;
-    if(arr[parent]<arr[index]) { // max heap
-      swap(arr[parent],arr[index]);
+void printGraph(){
+  for(auto it:mp){
+    cout<<it.first<<" : ";
+    for(int i:it.second) {
+      cout<<i<<" ";
     }
-    else return;
-    index = index/2;
-    
-    
-  }
-  
-}
-void del (){
-  if(size==0) {cout<<"Nothing to delete"<<endl;return; }
-  swap(arr[0],arr[size]);
-  arr.pop_back();
-  size--;
-  for(int i=1;i<size;i++){
-    int leftIndex = 2*i;
-    int rightIndex = 2*i+1;
-    if(leftIndex<size && arr[leftIndex] > arr[i]) {
-      swap(arr[leftIndex],arr[i]);
-    }
-    else if (rightIndex<size && arr[rightIndex] > arr[i]) {
-      swap(arr[rightIndex],arr[i]);
-    }
-    else { cout<<"Successfully Deleted"<<endl;return; }
+    cout<<endl;
   }
 }
-void print(){
-  for(int i=0;i<size;i++){
-    cout<<arr[i]<<" ";
-  }
-  cout<<endl;
-}
-
 };
 
-void heapify(vector<int>& arr, int n, int i) {
-    int largest = i;  // Initialize largest as root
-    int left = 2 * i ;  // left = 2*i + 1
-    int right = 2 * i + 1;  // right = 2*i + 2
-
-    // If left child is larger than root
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-
-    // If right child is larger than largest so far
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
-
-    // If largest is not root
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
-
-        // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
-    }
-}
-
-void heapSort(vector<int> &arr){
-    int size = arr.size();
-
-    // Build max heap
-    for (int i = size / 2 - 1; i >= 0; --i)
-        heapify(arr, size, i);
-
-    // Extract elements from heap one by one
-    for (int i = size - 1; i > 0; --i) {
-        // Move current root to end
-        swap(arr[0], arr[i]);
-
-        // call max heapify on the reduced heap
-        heapify(arr, i, 0);
-    }
-}
-
 int main() {
-    vector<int> arr = {55, 53, 54, 52, 50}; // Index 0 should not be used in heap sort
-    heapSort(arr);
-    for(int i : arr) {
-        cout << i << " ";
-    }
+    Graph *g = new Graph();
+    g->insertEdge(0,1,0);
+    g->insertEdge(1,2,0);
+    g->insertEdge(2,0,0);
+    g->printGraph();
     return 0;
 }
 
